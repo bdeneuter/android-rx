c93ec55d2d23f9b12f1412a6bb98e941c1d4e0ff
Bart De Neuter <bart.deneuter@cegeka.be>
2014-09-08 13:20:28 +0200

    made test green

--- app/src/androidTest/java/rx/android/cegeka/be/rx/AbstractTestCase.java
+++ app/src/androidTest/java/rx/android/cegeka/be/rx/AbstractTestCase.java
@@ -1,14 +1,15 @@
 package rx.android.cegeka.be.rx;
 
-import junit.framework.TestCase;
+import android.test.AndroidTestCase;
 
 import static org.mockito.MockitoAnnotations.initMocks;
 
-public class AbstractTestCase extends TestCase {
+public class AbstractTestCase extends AndroidTestCase {
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        System.setProperty("dexmaker.dexcache", getContext().getCacheDir().toString());
         initMocks(this);
     }
 }
