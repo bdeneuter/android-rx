71256f84e4ca576c2fbd8d7bc79c4043807ef136
Bart De Neuter <bart.deneuter@cegeka.be>
2014-09-08 15:31:24 +0200

    add Plane and position

---
+++ app/src/main/java/be/cegeka/android/rx/domain/Plane.java
@@ -0,0 +1,34 @@
+package be.cegeka.android.rx.domain;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Func1;
+
+public class Plane {
+
+    private Position position;
+
+    public Plane(Position initialPosition) {
+        position = initialPosition;
+    }
+
+    public Observable<Position> position() {
+        return Observable.create(new Observable.OnSubscribe<Position>() {
+            @Override
+            public void call(Subscriber<? super Position> subscriber) {
+                subscriber.onNext(position);
+                subscriber.onCompleted();
+            }
+        });
+    }
+
+    public static Func1<Plane, Observable<Position>> toPositions() {
+        return new Func1<Plane, Observable<Position>>() {
+            @Override
+            public Observable<Position> call(Plane plane) {
+                return plane.position();
+            }
+        };
+    }
+
+}
---
+++ app/src/main/java/be/cegeka/android/rx/domain/Position.java
@@ -0,0 +1,7 @@
+package be.cegeka.android.rx.domain;
+
+/**
+ * Created by bkbdn on 8/09/14.
+ */
+public class Position {
+}
