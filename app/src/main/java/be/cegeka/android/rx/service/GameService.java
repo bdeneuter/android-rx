d2088644db340f905276e7bdc6b68b9ae893269e
Bart De Neuter <bart.deneuter@cegeka.be>
2014-09-08 13:22:35 +0200

    break test again for exercise 1

--- app/src/main/java/be/cegeka/android/rx/service/GameService.java
+++ app/src/main/java/be/cegeka/android/rx/service/GameService.java
@@ -3,18 +3,11 @@ package be.cegeka.android.rx.service;
 
 import be.cegeka.android.rx.domain.Game;
 import rx.Observable;
-import rx.Subscriber;
 
 public class GameService {
 
     public Observable<Game> createNewGame() {
-        return Observable.create(new Observable.OnSubscribe<Game>() {
-            @Override
-            public void call(Subscriber<? super Game> subscriber) {
-                subscriber.onNext(new Game());
-                subscriber.onCompleted();
-            }
-        });
+        throw new IllegalStateException("Implement me please!!!!");
     }
 
 }
