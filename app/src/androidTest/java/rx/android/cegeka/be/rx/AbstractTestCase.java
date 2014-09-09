aaf42d79e0d1674086dc1d2b21a2fb587e47ccfb
Bart De Neuter <bart.deneuter@cegeka.be>
2014-09-09 15:31:13 +0200

    fix build

--- app/src/androidTest/java/rx/android/cegeka/be/rx/AbstractTestCase.java
+++ app/src/androidTest/java/rx/android/cegeka/be/rx/AbstractTestCase.java
@@ -2,26 +2,34 @@ package rx.android.cegeka.be.rx;
 
 import android.test.AndroidTestCase;
 
-import org.mockito.Mock;
-
 import be.cegeka.android.rx.infrastructure.RotationSensor;
 import rx.Observable;
 
-import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class AbstractTestCase extends AndroidTestCase {
 
-    @Mock
-    protected RotationSensor rotationSensorMock;
+    protected RotationSensor rotationSensorMock = new RotationSensor() {
+        @Override
+        public Observable<Double> connectX() {
+            return Observable.empty();
+        }
+
+        @Override
+        public Observable<Double> connectY() {
+            return Observable.empty();
+        }
+
+        @Override
+        public Observable<Double> connectZ() {
+            return Observable.empty();
+        }
+    };
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         System.setProperty("dexmaker.dexcache", getContext().getCacheDir().toString());
         initMocks(this);
-        when(rotationSensorMock.connectX()).thenReturn(Observable.<Double>empty());
-        when(rotationSensorMock.connectY()).thenReturn(Observable.<Double>empty());
-        when(rotationSensorMock.connectZ()).thenReturn(Observable.<Double>empty());
     }
 }
